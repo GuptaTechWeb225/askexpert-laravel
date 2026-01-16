5c7cf2e8ca3c95088508f4e6f85d5b02c0487345
gmahi5053 <gmahi5053@gmail.com>
2026-01-16 14:11:22 +0530

    cms update full

--- resources/js/chat-component.js
+++ resources/js/chat-component.js
@@ -1611,7 +1611,7 @@ export function adminExpertChatComponent() {
                             this.agoraClient = this.createAgoraClient();
                         }
 
-                        const res = await axios.post(`/expert/massages/admin-chat/${chatId}/generate-token`);
+                        const res = await axios.post(`/expert/massages/admin-chat/${this.selectedExpertId}/generate-token`);
                         const { token, channel, uid, app_id } = res.data;
 
                         await this.agoraClient.join(app_id || window.AGORA_APP_ID, channel, token, uid);
